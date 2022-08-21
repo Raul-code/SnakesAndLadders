fd9104689869dccd415a25cac7e9d5030fd7fdb2
Raúl Bea González <quake2222@hotmail.com>
2022-08-21 16:35:34 +0200

    added tests

--- SnakesAndLadders.Test/GameTests.cs
+++ SnakesAndLadders.Test/GameTests.cs
@@ -78,5 +78,49 @@ namespace SnakesAndLadders.Test
             Assert.Equal(100, game.Players[0].Position);
             Assert.True(game.IsFinished);
         }
+
+        [Fact]
+        public async Task CheckPositionBiggerThanLastSquareGame()
+        {
+            var diceServiceMock = new Mock<IDiceService>();
+            var nextTurnServiceMock = new Mock<INextTurnService>();
+            var drawServiceMock = new Mock<IDrawService>();
+
+            TaskCompletionSource taskCompletionSource = new();
+            diceServiceMock.SetupSequence(x => x.RollsDice()).Returns(4);
+            nextTurnServiceMock.SetupSequence(x => x.NextTurnAsync()).ReturnsAsync(true).ReturnsAsync(false);
+
+            var game = new Game(nextTurnServiceMock.Object, drawServiceMock.Object, diceServiceMock.Object, boardFileName);
+            await game.Initialize(2);
+
+            game.Players[0].Position = 97;
+
+            await game.StartGame();
+
+            Assert.Equal(97, game.Players[0].Position);
+        }
+
+        [Fact]
+        public async Task CheckPlayerMovement()
+        {
+            var diceServiceMock = new Mock<IDiceService>();
+            var nextTurnServiceMock = new Mock<INextTurnService>();
+            var drawServiceMock = new Mock<IDrawService>();
+
+            TaskCompletionSource taskCompletionSource = new();
+            diceServiceMock.SetupSequence(x => x.RollsDice()).Returns(4);
+            nextTurnServiceMock.SetupSequence(x => x.NextTurnAsync()).ReturnsAsync(true).ReturnsAsync(false);
+
+            var game = new Game(nextTurnServiceMock.Object, drawServiceMock.Object, diceServiceMock.Object, boardFileName);
+            await game.Initialize(2);
+
+            var initialPosition = game.Players[0].Position;
+
+            game.Players[0].Position = 1;
+
+            await game.StartGame();
+
+            Assert.Equal(initialPosition + 4, game.Players[0].Position);
+        }
     }
 }
